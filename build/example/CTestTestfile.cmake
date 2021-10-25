92b2ed3cec45be94686075a36bd380a54190b7b0
mikeyang1015 <408500550@gms.tku.edu.tw>
2021-10-25 22:53:43 +0800

    create new file

---
+++ build/example/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mike/python_test/src/example
+# Build directory: /home/mike/python_test/build/example
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
